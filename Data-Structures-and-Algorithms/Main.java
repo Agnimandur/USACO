05bfa241a420ecc4d27082ba9fb3edc67678f77b
Shiva2day <shivarasul@gmail.com>
2020-06-15 15:32:57 -0700

    Added sorting code and a Node class
    Update 1: I added some very useful methods to shuffle and sort 2D arrays. These codes have a built in comparator and pre-shuffle the array, to avoid Quicksort TLEs.
    
    Update 2: I added my experimental Node class, which uses a HashSet to allow for graphs to be quickly modified and queried. It is quite slow though, so only use it when removing nodes is key.

--- Data-Structures-and-Algorithms/Main.java
+++ Data-Structures-and-Algorithms/Main.java
@@ -5,17 +5,59 @@ class Main {
 
   //USEFUL METHODS
 
+  public static int[][] sort(int[][] arr) {
+    //Sort an array (immune to quicksort TLE)
+		Random rgen = new Random();
+		for (int i = 0; i < 3*arr.length; i++) {
+      int randomPosition = rgen.nextInt(arr.length);
+      int[] temp = arr[i];
+      arr[i] = arr[randomPosition];
+      arr[randomPosition] = temp;
+		}
+    Arrays.sort(arr, new Comparator<int[]>() {
+      @Override
+      public int compare(int[] arr1, int[] arr2) {
+        return arr1[1]-arr2[1];
+        //Ascending order.
+      }
+    });
+    return arr;
+  }
+
+  public static long[][] sort(long[][] arr) {
+    //Sort an array (immune to quicksort TLE)
+		Random rgen = new Random();
+		for (int i = 0; i < 3*arr.length; i++) {
+      int randomPosition = rgen.nextInt(arr.length);
+      long[] temp = arr[i];
+      arr[i] = arr[randomPosition];
+      arr[randomPosition] = temp;
+		}
+    Arrays.sort(arr, new Comparator<long[]>() {
+      @Override
+      public int compare(long[] arr1, long[] arr2) {
+        if (arr1[0] > arr2[0])
+          return 1;
+        else
+          return -1;
+
+        //Ascending order.
+      }
+    });
+    return arr;
+  }
+
   //Fast exponentiation (x^y mod m)
-  public static long power(long x, long y, long m) { 
+  public static long power(long x, long y, long m) {
     long ans = 1;
     x %= m;
-    while (y > 0) { 
-      if(y % 2 == 1) 
-        ans = (ans * x) % m; 
-      y /= 2;  
+    while (y > 0) {
+      if(y % 2 == 1)
+        ans = (ans * x) % m;
+      y /= 2;
       x = (x * x) % m;
-    } 
-    return ans; 
+    }
+    return ans;
   }
 
   //Find the distance between two points
---
+++ Data-Structures-and-Algorithms/Node.java
@@ -0,0 +1,33 @@
+//An experimental Node class
+public class Node {
+	public HashSet<Node> children;
+	public int n;
+	
+	public Node(int n) {
+		this.n = n;
+		children = new HashSet<Node>();
+	}
+	
+	public void addChild(Node node) {
+		children.add(node);
+	}
+	
+	public void removeChild(Node node) {
+		children.remove(node);
+	}
+  
+	@Override
+	public int hashCode() {
+		return n;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (! (obj instanceof Node)) {
+			return false;
+		} else {
+			Node node = (Node) obj;
+			return (n == node.n);
+		}
+	}
+}
